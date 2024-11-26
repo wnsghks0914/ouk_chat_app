0eb87304c5f2eafa1410219c5554bded7e0dba41
김의현(toma_to) <148212609+tomaho1756@users.noreply.github.com>
2024-11-26 14:16:24 +0900

    initialCommit

---
+++ ouk_chat/android/app/src/main/kotlin/com/example/ouk_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ouk_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ouk_chat/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'controllers/room_list_controller.dart';
+import 'views/room_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+    );
+  }
+}
